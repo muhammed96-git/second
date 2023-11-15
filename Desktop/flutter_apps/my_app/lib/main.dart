ed46846826f69802c5524431ea26c219fbd663ea
yasinabdmahmood <abd181103@gmail.com>
2023-11-16 00:54:55 +0300

    Reformat the displayed test to look abit nicer in the center of the screen

--- Desktop/flutter_apps/my_app/lib/main.dart
+++ Desktop/flutter_apps/my_app/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text("Hello"),));
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("Hello"),
+      ),
+    ),
+  ));
 }
-
