b19ed744579847e3ca9a844600664ef6d4b7abb4
yasinabdmahmood <abd181103@gmail.com>
2023-11-16 01:10:05 +0300

    Add backgroud color

--- Desktop/flutter_apps/my_app/lib/main.dart
+++ Desktop/flutter_apps/my_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Text("Hello"),
       ),
